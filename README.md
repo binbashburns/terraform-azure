d5244acf1cfe188448563d378c57886e43206dc3
matt <pro.mattburns@gmail.com>
2022-01-14 09:15:40 -0500

    Modified main.tf and README.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Terraform Project
-This is Matt's first project in Terraform!
+## This is Matt's first Azure project in Terraform!
+This project creates the following resources:
+- Resource Group (tf-rg)
+- Virtual Network (tf-vnet, 10.0.0.0/16)
+  - Subnet (tf-sn, 10.0.2.0/24)
+  - Network Interface (Internal, DHCP)
+- Virtual Machine running Microsoft Windows Server 2016 Datacenter
+## Default credentials (change these!)
+Username: tf-admin
+Password: @pplyYourself123!
--- main.tf
+++ main.tf
@@ -1,5 +1,3 @@
-# This is my first Terraform configuration!
-
 # This is my first Azure Terraform configuration!
 
 # Provides configuration details for Terraform
@@ -12,7 +10,8 @@ terraform {
   }
 }
 
-# Provides configuration details for the Azure Terraform provider
+# Provides configuration details for the Azure Terraform provider. 
+# We don't have any features in this deployment, but if the features block isn't present, we get an error.
 provider "azurerm" {
   features {}
 }
@@ -21,8 +20,56 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "tf-rg"
   location = "eastus"
-  tags = {
-    environment = "dev"
-    source      = "Terraform"
+}
+
+# Provides Virtual Network 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tf-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Provides internal subnet within the Virtual Network for VM
+resource "azurerm_subnet" "sn" {
+  name                 = "tf-sn"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Provides NIC for VM to connect to internal subnet
+resource "azurerm_network_interface" "nic" {
+  name                = "tf-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sn.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Provides Windows Virtual Machine in resource group
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                  = "tf-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  size                  = "Standard_DS1_v2"
+  admin_username        = "tf-admin"
+  admin_password        = "@pplyYourself123!"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
   }
 }
