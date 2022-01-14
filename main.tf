b232597fa1b8af42c6e7ce0a043aa29dcd3a5a8c
matt <33589090+binbashburns@users.noreply.github.com>
2022-01-14 07:03:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,28 @@
 # This is my first Terraform configuration!
+
+# This is my first Azure Terraform configuration!
+
+# Provides configuration details for Terraform
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.91.0"
+    }
+  }
+}
+
+# Provides configuration details for the Azure Terraform provider
+provider "azurerm" {
+  features {}
+}
+
+# Provides the Resource Group to logically contain resources
+resource "azurerm_resource_group" "rg" {
+  name     = "tf-rg"
+  location = "eastus"
+  tags = {
+    environment = "dev"
+    source      = "Terraform"
+  }
+}
